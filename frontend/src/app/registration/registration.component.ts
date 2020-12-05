0c82886c12f0b5d1fefb0862d773dc05d6e30d8d
Ante Vuletić Antić <ante.antic@kiwi.com>
2020-12-05 18:01:29 +0100

    add fe registration view

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+    selector: 'app-registration',
+    templateUrl: './registration.component.html',
+    styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+    hide = true;
+    rootURL = 'http://localhost:8080';
 
-  constructor() { }
+    // tslint:disable-next-line:variable-name
+    constructor(private _snackBar: MatSnackBar, private _http: HttpClient, private _router: Router) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    onSubmit(data): void {
+        this._http.post<any>(this.rootURL + '/api/v1/registration', data).subscribe(resp => {
+                this._snackBar.open('Registration complete!', 'OK', {duration: 2000});
+                this._router.navigateByUrl('/login');
+            },
+            error => {
+                this._snackBar.open('Bad request', 'OK', {duration: 2000});
+            }
+        );
+    }
 
 }
--- frontend/src/app/toolbar/toolbar.component.ts
+++ frontend/src/app/toolbar/toolbar.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationStart, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-toolbar',
-  templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.css']
+    selector: 'app-toolbar',
+    templateUrl: './toolbar.component.html',
+    styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
+    loggedIn: boolean;
 
-  constructor() { }
+    // tslint:disable-next-line:variable-name
+    constructor(private _router: Router) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this._router.events.subscribe(event => {
+            if (event instanceof NavigationStart) {
+                this.loggedIn = localStorage.getItem('auth') != null;
+            }
+        });
+    }
+
+    onLogout(): void {
+        localStorage.removeItem('auth');
+        this.loggedIn = false;
+    }
 
 }
